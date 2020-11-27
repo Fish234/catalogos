689240ce1f8e7e7415ab50d016a3ab466a18f883
Natalia <hourglassbranch25@gmail.com>
2020-11-27 04:24:27 -0300

    end

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
+  resources :oneclicks
+  resources :webpays
+  resources :credit_cards
+  resources :transbanks
+  resources :payments
+  resources :orders
+  resources :paypals
+  resources :stripes
+  resources :images
+  resources :physicals
+  resources :digitals
+  resources :products
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
